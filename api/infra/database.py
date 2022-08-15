3686805ebc74641d74a53c804fc99e4e3753624c
Yan Cleiton <yancleiton7@hotmail.com>
2022-08-14 22:01:19 -0300

    abc

--- api/infra/database.py
+++ api/infra/database.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///eletrocell.db"
+
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
